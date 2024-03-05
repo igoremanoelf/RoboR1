c1686e4e291b874b1e335ae5c15364be318a4fe1
tarde.cetafaju <tarde.cetafaju@PC03LAB2536>
2024-03-05 15:26:27 -0300

    Adicionar arquivos de projeto.

---
+++ RoboScraper/ComparacaoDePrecos/ComparacaoPreco.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboR1.ComparacaoDePrecos
+{
+    internal class ComparacaoPreco
+    {
+        public static string CompararPreco(string precoMerL, string precoMagL, string linkMag, string linkMer)
+        {
+            char[] removerCaracteres = { 'R', '$', ' ' };
+
+            double precoMagazine = Convert.ToDouble(precoMagL.Trim(removerCaracteres));
+            double precoMercado = Convert.ToDouble(precoMerL.Trim(removerCaracteres));
+
+            Console.WriteLine(precoMercado);
+            Console.WriteLine(precoMagazine);
+
+            if (precoMagazine > precoMercado)
+            {
+                return "Melhor preço: Mercado Livre\n" + $"Link do produto:{linkMer}";
+                
+            }
+            else if (precoMercado > precoMagazine)
+            {
+                return "Melhor preço: Magazine Luíza\n" + $"Link do produto:{linkMag}";
+            }
+            else
+            {
+                return "Preços iguais";
+            }
+        }
+    }
+}
